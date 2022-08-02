278946ee3ebb39765fbcef00ed5cd4aa9a23aa35
Precious <Preshomondi@gmail.com>
2022-08-02 08:08:51 +0300

    Add ReadMe and add functionality to star icon

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
+#Weather Application
+
+##API Used:
+https://www.weatherapi.com/
+End points:
+
+
+##Application Description
 -The weather application is made up of five main components: Home, Weather, Forecast, Clock, and Navbar.
--The home page contains three buttons that each route to their respective pages.
-The Navbar appears on the Weather, Forecast and Clock pages. It does not appear on the home page
--The Weather page displays the current temperature in Celsius and the date. On the side, there are other weather details such as wind speed, visibility, humidity, and precipitation. The background image on the page changes based on the weather of the different searched cities. There is also a weather icon on the page that changes to match the weather description at the time.
--The forecast page 
+###Home page 
+-contains three buttons that each route to their respective pages.
+###Navbar 
+-Appears on the Weather, Forecast and Clock pages but not on the Home page.
+-Contains a search bar that displays options after inputing at least 3 characters
+###Weather 
+-Displays the current temperature in Celsius and the date. On the side, there are other weather details such as wind speed, visibility, humidity, and precipitation. 
+-The background image on the page changes based on the weather of the different searched cities. 
+-There is also a weather icon on the page that changes to match the weather description at the time.
+###Forecast 
+-Contains three sections:
+i)A main that shows the current weather of the searched city. This section displays the city's name and by default the city's temperature. At the bottom of that section is a navigation bar containing icons that represent different weather details.
+ii) 
 The clock page shows the different times for the sunrise, sunset, moonrise, and moonset for any searched city.
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,17 +1,61 @@
-import React, {useState} from "react"
+import React, { useEffect, useState } from "react"
 import "./Forecast.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faDroplet, faThermometer, faWind, faEye, faStar } from "@fortawesome/free-solid-svg-icons"
 
 function Forecast({ data }) {
-    
+
     const forecast = data.forecast.forecastday[0].hour
-    let hour = parseInt(data.current.last_updated.slice(data.current.last_updated.length - 5).slice(0,2))
+    let hour = parseInt(data.current.last_updated.slice(data.current.last_updated.length - 5).slice(0, 2))
     hour = hour < 20 ? hour : 20
 
     const [isActive, setIsActive] = useState(faThermometer)
     const [activeIcons, setActiveIcon] = useState([faThermometer, faDroplet, faEye, faWind])
-    
+    const [faveCity, setFaveCity] = useState([])
+    const [faveWeather, setFaveWeather] = useState([])
+
+    function handleFaveClick() {
+        const serverOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ id: "3", faveCity: data.location.name })
+        }
+        fetch("https://weather-cities-api.herokuapp.com/cities", serverOptions)
+            .then(r => r.json())
+            .then(data => {
+                console.log("i am here", data)
+            })
+            setFaveWeather(fave=> [...fave, data])
+    }
+
+    useEffect(() => {
+        fetch(`https://weather-cities-api.herokuapp.com/cities`)
+            .then(r => r.json())
+            .then(data => {
+                console.log("me", data)
+                setFaveCity(data)
+            })
+
+    }, [])
+
+    useEffect(() => {
+        faveCity.forEach((fave) => {
+            fetch(`http://api.weatherapi.com/v1/forecast.json?key=f2397f2d2d31453d8a2182757222907&q=${fave.faveCity}&aqi=yes`)
+                .then(r => r.json())
+                .then(data => {
+                    console.log("this", data)
+                    setFaveWeather((faveWeather) => {
+                        return [...faveWeather, data]
+                    })
+                })
+        })
+    }, [faveCity])
+    console.log("this", faveWeather)
+
+
+
     return (
         <div className="prediction" style={{ backgroundImage: `url("https://images.pexels.com/photos/11724626/pexels-photo-11724626.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"` }}>
             <div className="prediction-blur">
@@ -19,39 +63,39 @@ function Forecast({ data }) {
                 {/* prediction main */}
                 <div className="prediction-main">
                     <div className="star">
-                        <FontAwesomeIcon icon={faStar} />
+                        <FontAwesomeIcon icon={faStar} onClick={handleFaveClick} />
 
                     </div>
 
                     <h2>{data.location.name}</h2>
-                    <h1>{isActive === faDroplet ? data.current.humidity + "%":
-                              isActive === faThermometer ? data.current.temp_c  + "°":
-                              isActive === faWind ? data.current.wind_kph + "kph": 
-                              data.current.vis_km + "km"}</h1>
+                    <h1>{isActive === faDroplet ? data.current.humidity + "%" :
+                        isActive === faThermometer ? data.current.temp_c + "°" :
+                            isActive === faWind ? data.current.wind_kph + "kph" :
+                                data.current.vis_km + "km"}</h1>
                     <div className="icon">
-                        <img src={"svg/wi-" + data.current.condition.code + "-" + data.current.is_day + ".svg"} 
-                        style={{filter: `${data.current.is_day ? "invert(92%) sepia(41%) saturate(1655%) hue-rotate(333deg) brightness(100%) contrast(99%)": " invert(44%) sepia(40%) saturate(1429%) hue-rotate(184deg) brightness(88%) contrast(88%)"}`}}/>
+                        <img src={"svg/wi-" + data.current.condition.code + "-" + data.current.is_day + ".svg"}
+                            style={{ filter: `${data.current.is_day ? "invert(92%) sepia(41%) saturate(1655%) hue-rotate(333deg) brightness(100%) contrast(99%)" : " invert(44%) sepia(40%) saturate(1429%) hue-rotate(184deg) brightness(88%) contrast(88%)"}` }} />
                     </div>
 
                     <div className="main-details">
-                        {activeIcons.filter(icon => icon !== isActive ).map((icon, index) => {
-                            
-                            return(
-                                <div style={{display: "flex", }}><div className="">
-                                <p>{icon === faDroplet ? "Humidity" :
-                                icon === faThermometer ? "Temperature" :
-                                icon === faWind ? "Wind Speed": 
-                                "Visibility" }</p>
-                                <h3>{icon === faDroplet ? data.current.humidity + "%":
-                                icon === faThermometer ? data.current.temp_c  + "°":
-                                icon === faWind ? data.current.wind_kph + "km/hr": 
-                                data.current.vis_km + "km"}</h3>
-                                
-                             
-                            </div> {index < 2 ? <div className="first-line"></div>: null}</div>)
-                              
+                        {activeIcons.filter(icon => icon !== isActive).map((icon, index) => {
+
+                            return (
+                                <div style={{ display: "flex", }}><div className="">
+                                    <p>{icon === faDroplet ? "Humidity" :
+                                        icon === faThermometer ? "Temperature" :
+                                            icon === faWind ? "Wind Speed" :
+                                                "Visibility"}</p>
+                                    <h3>{icon === faDroplet ? data.current.humidity + "%" :
+                                        icon === faThermometer ? data.current.temp_c + "°" :
+                                            icon === faWind ? data.current.wind_kph + "km/hr" :
+                                                data.current.vis_km + "km"}</h3>
+
+
+                                </div> {index < 2 ? <div className="first-line"></div> : null}</div>)
+
                         })}
-                      
+
 
                         {/* <div className="humidity">
                             <p>Humidity</p>
@@ -69,18 +113,18 @@ function Forecast({ data }) {
                         </div> */}
                     </div>
                     <div className="main-navbar">
-                        {activeIcons.map((icon)=>  {
-                            function handleActiveClick(){
+                        {activeIcons.map((icon) => {
+                            function handleActiveClick() {
                                 setIsActive(icon)
                             }
-                            return(
-                             <FontAwesomeIcon icon={icon} className={icon === isActive ? 'navbar-icon active' : 'navbar-icon'} onClick={handleActiveClick} />
+                            return (
+                                <FontAwesomeIcon icon={icon} className={icon === isActive ? 'navbar-icon active' : 'navbar-icon'} onClick={handleActiveClick} />
                             )
 
                         }
-                           
+
                         )}
-                        
+
                         {/* <FontAwesomeIcon icon={faDroplet} className={isActive ? 'navbar-icon active' : 'navbar-icon'} onClick={handleActiveClick} />
                         <FontAwesomeIcon icon={faWind} className={isActive ? 'navbar-icon active' : 'navbar-icon'} onClick={handleActiveClick} />
                         <FontAwesomeIcon icon={faEye} className={isActive ? 'navbar-icon active' : 'navbar-icon'} onClick={handleActiveClick} /> */}
@@ -88,25 +132,25 @@ function Forecast({ data }) {
                 </div>
                 {/* prediction side */}
                 <div className="prediction-side">
-                    {forecast.slice(hour, hour+4).map((box) => {
-                        return(
+                    {forecast.slice(hour, hour + 4).map((box) => {
+                        return (
                             <div className="prediction-side-box one">
-                            <h2>{box.time.slice(-5)}</h2>
-                            <p>{Date(box.time_epoch).slice(0,10)}</p>
-                            <div className="icon">
-                                <img src={"svg/wi-" + box.condition.code + "-" + box.is_day + ".svg"} 
-                                style={{filter: `${box.is_day ? "invert(92%) sepia(41%) saturate(1655%) hue-rotate(333deg) brightness(100%) contrast(99%)": " invert(44%) sepia(40%) saturate(1429%) hue-rotate(184deg) brightness(88%) contrast(88%)"}`}}/>
+                                <h2>{box.time.slice(-5)}</h2>
+                                <p>{Date(box.time_epoch).slice(0, 10)}</p>
+                                <div className="icon">
+                                    <img src={"svg/wi-" + box.condition.code + "-" + box.is_day + ".svg"}
+                                        style={{ filter: `${box.is_day ? "invert(92%) sepia(41%) saturate(1655%) hue-rotate(333deg) brightness(100%) contrast(99%)" : " invert(44%) sepia(40%) saturate(1429%) hue-rotate(184deg) brightness(88%) contrast(88%)"}` }} />
+                                </div>
+                                <div className="shadow"></div>
+                                <div className="prediction-side-details">
+                                    <p>{box.temp_c}° C</p>
+                                    <p>{box.condition.text}</p>
+                                </div>
                             </div>
-                            <div className="shadow"></div>
-                            <div className="prediction-side-details">
-                                <p>{box.temp_c}° C</p>
-                                <p>{box.condition.text}</p>
-                            </div>
-                        </div>
 
                         )
                     })}
-                   
+
                     {/* <div className="prediction-side-box two">
                         <h2>Monday</h2>
                         <p>24th July</p>
@@ -146,30 +190,20 @@ function Forecast({ data }) {
                 </div>
 
                 <div className="prediction-side-bottom">
-                    <div className="side-bottom-city one">
+                    {faveWeather.slice(-3).map((weather) => {
+                        return (
+                            <div className="side-bottom-city one">
+                                <h2>{weather.location.name}</h2>
+                                <div className="icon">
+                                    <img src={"svg/wi-" + weather.current.condition.code + "-" + weather.current.is_day + ".svg"}
+                                        style={{ filter: `${data.current.is_day ? "invert(92%) sepia(41%) saturate(1655%) hue-rotate(333deg) brightness(100%) contrast(99%)" : " invert(44%) sepia(40%) saturate(1429%) hue-rotate(184deg) brightness(88%) contrast(88%)"}` }} />
+                                </div>
+                                <h3>{weather.current.temp_c}&deg;C</h3>
+                            </div>
+                        )
+                    })}
 
-
-                        <h2>Bei jing</h2>
-                        <div className="icon">
-                            <img src={"svg/wi-" + data.current.condition.code + "-" + data.current.is_day + ".svg"} 
-                            style={{filter: `${data.current.is_day ? "invert(92%) sepia(41%) saturate(1655%) hue-rotate(333deg) brightness(100%) contrast(99%)": " invert(44%) sepia(40%) saturate(1429%) hue-rotate(184deg) brightness(88%) contrast(88%)"}`}}/>
-                        </div>
-                        <h3>28°C</h3>
-
-
-                    </div>
-                    <div className="side-bottom-city two">
-
-
-                        <h2>Bei jing</h2>
-                        <div className="icon">
-                            <img src={"svg/wi-" + data.current.condition.code + "-" + data.current.is_day + ".svg"} />
-                        </div>
-                        <h3>28°C</h3>
-
-
-                    </div>
-                    <div className="side-bottom-city three">
+                    {/* <div className="side-bottom-city two">
 
 
                         <h2>Bei jing</h2>
@@ -179,7 +213,18 @@ function Forecast({ data }) {
                         <h3>28°C</h3>
 
 
-                    </div>
+                    </div> */}
+                    {/* <div className="side-bottom-city three">
+
+
+                        <h2>Bei jing</h2>
+                        <div className="icon">
+                            <img src={"svg/wi-" + data.current.condition.code + "-" + data.current.is_day + ".svg"} />
+                        </div>
+                        <h3>28°C</h3>
+
+
+                    </div> */}
                 </div>
 
             </div>
